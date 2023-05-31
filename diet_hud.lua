d1ed9f9c72c213101da11adf2a04338c1af4a75f
flux <25628292+fluxionary@users.noreply.github.com>
2023-05-31 16:05:29 -0700

    sort things in diet hud by local language code if it's defined

--- diet_hud.lua
+++ diet_hud.lua
@@ -1,5 +1,6 @@
 local f = string.format
 
+-- TODO: this coloring stuff doesn't work because HUDs don't support it :(
 local function get_food_color(percent)
 	local red, green
 	if percent > 0.5 then
@@ -18,8 +19,15 @@ balanced_diet.diet_hud = futil.define_hud("balanced_diet:diet", {
 	get_hud_def = function(player)
 		local now = os.time()
 		local eaten = balanced_diet.get_eaten(player, now)
+		local player_name = player:get_player_name()
+		local lang_code = minetest.get_player_information(player_name).lang_code
 		local lines = {}
-		local sort_order_by_description = {}
+		local function sort_key(value)
+			return minetest.get_translated_string(lang_code, minetest.strip_colors(value)):lower()
+		end
+		local function cmp(a, b)
+			return sort_key(a) < sort_key(b)
+		end
 
 		for item, remaining in pairs(eaten) do
 			local food_def = balanced_diet.get_food_def(item)
@@ -30,18 +38,19 @@ balanced_diet.diet_hud = futil.define_hud("balanced_diet:diet", {
 				f("%s: %.1f%%", description, 100 * remaining_percent)
 			)
 			lines[#lines + 1] = description
-			sort_order_by_description[description] = minetest.strip_colors(futil.strip_translation(description))
 		end
 
-		table.sort(lines, function(a, b)
-			return sort_order_by_description[a]:lower() < sort_order_by_description[b]:lower()
-		end)
+		table.sort(lines, cmp)
 
 		if #lines > 0 then
 			table.insert(lines, "---------------")
 		end
 
-		for nutrient, def in futil.table.pairs_by_key(balanced_diet.registered_nutrients) do
+		for nutrient, def in
+			futil.table.pairs_by_value(balanced_diet.registered_nutrients, function(a, b)
+				return cmp(a.description, b.description)
+			end)
+		do
 			local value = balanced_diet.check_nutrient_value(player, nutrient, now)
 			lines[#lines + 1] = f("%s: %.1f", def.description, value)
 		end
